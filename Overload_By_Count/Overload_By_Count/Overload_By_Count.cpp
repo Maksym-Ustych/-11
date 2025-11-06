19f817369a148f7ac2d6fcd795bf47cb7053a78a
Максм <m.ustich@istu.edu.ua>
2025-11-06 20:00:35 +0200

    додано самостійну №11

---
+++ Overload_By_Count/Overload_By_Count/Overload_By_Count.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+// --- Перевантаження за кількістю аргументів ---
+int sum(int a, int b) {
+    return a + b;
+}
+
+int sum(int a, int b, int c) {
+    return a + b + c;
+}
+
+int main() {
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
+    cout << "--- Перевантаження за кількістю аргументів ---" << endl;
+    cout << "sum(4, 6) = " << sum(4, 6) << endl;
+    cout << "sum(2, 3, 5) = " << sum(2, 3, 5) << endl;
+
+    return 0;
+}
---
+++ Overload_By_Type/Overload_By_Type/Overload_By_Type.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+// --- Перевантаження за типом аргументів ---
+void show(int x) {
+    cout << "Функція show(int): x = " << x << endl;
+}
+
+void show(double x) {
+    cout << "Функція show(double): x = " << x << endl;
+}
+
+void show(char x) {
+    cout << "Функція show(char): x = '" << x << "'" << endl;
+}
+
+int main() {
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
+    cout << "--- Перевантаження за типом аргументів ---" << endl;
+    show(15);
+    show(2.71);
+    show('Б');
+
+    return 0;
+}
